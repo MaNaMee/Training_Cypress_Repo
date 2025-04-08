84c3420656c46d11dfb38cb61a21b02ae4276fe3
MaNaMee <joshuaazarcon22@gmail.com>
2025-04-08 10:31:26 +0800

    Configured a CI/CD test for a test cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
+// npx cypress run --record --key 86c44907-cfd3-48bb-b704-7be82ca63df9
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "372u8v",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
