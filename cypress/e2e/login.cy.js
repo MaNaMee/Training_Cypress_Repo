b1851616d5476dd509aedb43044bf1d45a4f721f
MaNaMee <joshuaazarcon22@gmail.com>
2025-04-08 16:56:05 +0800

    First Cypress Automation Script (Ongoing)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,43 +23,7 @@ describe('login function testing - ', () => {
     cy.contains('Epic sadface').should('be.visible')
     cy.url().should('include','saucedemo.com')
 
-
-    describe('Authentication & Add to Cart', () => {
-      // It will run before each test case (it block)
-     /*if matagal mag run ang test
-      * 1.possible na slow internet 
-        2.mabagal ang test environment
-        3. down yung Server (API, DB etc.)*/
-     beforeEach(() => {
-       cy.auth('standard_user', 'secret_sauce')
-     });
-   
-     it('Should successfully login', () => {
-       // Verify we're on the inventory page after login
-       cy.url().should('include', '/inventory.html')
-       cy.get('.inventory_list').should('be.visible')
-     });
-   
-     it('Should successfully add to cart', () => {
-       // Add first product to cart
-       cy.get('[data-test="add-to-cart-sauce-labs-backpack"]')
-       .should('be.visible')
-       .click()
-   
-       // Verify cart badge appears with 1 item
-       cy.get('.shopping_cart_badge').should('contain', '1')
-   
-       // Optionally, navigate to the cart and verify item is listed
-       cy.get('.shopping_cart_link').click()
-       cy.url().should('include', '/cart.html')
-       cy.get('.cart_item').should('have.length', 1)
-       cy.get('.inventory_item_name').should('contain', 'Sauce Labs Backpack')
-     });
-
-     it('Should successfully checkout an item', () => {
-        
-
-     })
-   });  
   })
-})
+    
+  })
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,6 +28,42 @@ Cypress.Commands.add('auth', (username, password) => { // FUNCTION OR METHOD -->
     cy.visit('https://www.saucedemo.com/', {timeout: 240000})
       cy.get('[data-test="username"]').type(username)
       cy.get('[data-test="password"]').type(password)
-      cy.get('[data-test="login-button"]').click()
+      cy.get('[data-test="login-button"]').click()  
 }); // Try to create other Commands - pwedeng yung add to cart at checkout is gagawan natin ng commands
  
+
+Cypress.Commands.add('AddToCart', () => {
+  //cy.auth('standard_user', 'secret_sauce')
+  //cy.visit('https://www.saucedemo.com/inventory.html', {timeout: 240000})
+  cy.get('[data-test="item-0-title-link"] > [data-test="inventory-item-name"]').click();
+  cy.contains('Sauce Labs Bike Light').should('be.visible')
+  cy.get('[data-test="add-to-cart"]').should('be.visible').should('not.be.disabled').click()
+  cy.get('.shopping_cart_badge').should('contain', '1')
+  cy.get('[data-test="shopping-cart-link"]').click()
+  cy.get('.cart_item').should('have.length', 1)
+  cy.get('.inventory_item_name').should('contain', 'Sauce Labs Bike Light')
+});
+
+Cypress.Commands.add('Checkout', () => {
+  //cy.auth('standard_user', 'secret_sauce')
+  //cy.visit('https://www.saucedemo.com/inventory.html', {timeout: 240000})
+  //cy.AddToCart()
+  cy.get('[data-test="shopping-cart-link"]').click()
+  cy.get('[data-test="title"]').should('be.visible')
+  cy.contains('Your Cart').should('be.visible')
+  cy.contains('Sauce Labs Bike Light').should('be.visible')
+  cy.get('[data-test="checkout"]').should('not.be.disabled').click()
+  cy.get('[data-test="title"]').should('be.visible')
+  cy.contains('Checkout: Your Information').should('be.visible')
+  cy.get('[data-test="firstName"]').should('be.visible').type('Juan')
+  cy.get('[data-test="lastName"]').should('be.visible').type('Dela Cruz')
+  cy.get('[data-test="postalCode"]').should('be.visible').type('1116')
+  cy.get('[data-test="continue"]').should('not.be.disabled').click()
+  cy.contains('Checkout: Overview').should('be.visible')
+  cy.get('[data-test="finish"]').should('be.visible').click()
+  cy.get('[data-test="title"]').should('be.visible')
+  cy.contains('Checkout: Complete!').should('be.visible')
+  cy.get('[data-test="pony-express"]').should('be.visible')
+  cy.contains('Thank you for your order!').should('be.visible')
+  cy.get('[data-test="back-to-products"]').should('be.visible').click()
+})
