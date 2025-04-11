15f2ae1837f14296018948a72d684bb441d4fe80
MaNaMee <joshuaazarcon22@gmail.com>
2025-04-11 10:50:12 +0800

    Updated command.js, utils.js , created ParaBank_regis_test.js , and modified package.json to implement fixtures and faker.js

---
+++ cypress/e2e/ParaBank_Regis_Test.cy.js
@@ -0,0 +1,174 @@
+import { generateCustomerData } from "../support/utils"
+
+// describe('Parabank User Registration', () => {
+//     before(() => {
+//         const randomUsername = `User${Math.floor(Math.random() * 10000)}`
+//         const randompword =`pword${Math.floor(Math.random() * 10000)}`
+
+//         Cypress.env('randomusername', randomUsername)
+//         Cypress.env('randompword' , randompword)
+//     })
+    
+//     beforeEach(() =>{
+
+//         // const randomUsername = `User${Math.floor(Math.random() * 10000)}`
+//         // const randompword =`pword${Math.floor(Math.random() * 10000)}`
+
+//         // Cypress.env('randomusername', randomUsername)
+//         // Cypress.env('randompword' , randompword)
+
+//         cy.visit('https://parabank.parasoft.com/parabank/register.htm')
+//     })
+
+//     it('Should successfully create a user', () => {
+
+//         const session_username = Cypress.env('randomusername')
+//         const session_pword = Cypress.env('randompword')
+        
+//         cy.url().should('include','register.htm')
+//         cy.get('.title').should('be.visible')
+//         cy.contains('First Name').should('be.visible')    
+//         cy.get('input[id="customer.firstName"]').should('be.visible').type('Adele')
+//         cy.contains('Last Name').should('be.visible')
+//         cy.get('input[id="customer.lastName"]').should('be.visible').type('Singer')
+//         cy.contains('Address').should('be.visible')
+//         cy.get('input[id="customer.address.street"]').should('be.visible').type('LV Leviste')
+//         cy.contains('City').should('be.visible')
+//         cy.get('input[id="customer.address.city"]').should('be.visible').type('Makati')
+//         cy.contains('State').should('be.visible')
+//         cy.get('input[id="customer.address.state"]').should('be.visible').type('NCR')
+//         cy.contains('Zip Code').should('be.visible')
+//         cy.get('input[id="customer.address.zipCode"]').should('be.visible').type('12345')
+//         cy.contains('Phone #').should('be.visible')
+//         cy.get('input[id="customer.phoneNumber"]').should('be.visible').type('123456789')
+//         cy.contains('SSN').should('be.visible')
+//         cy.get('input[id="customer.ssn"]').should('be.visible').type('238756983645')
+//         cy.contains('Username:').should('be.visible')
+//         cy.get('input[id="customer.username"]').should('be.visible').type(session_username)
+//         cy.contains('Password').should('be.visible')
+//         cy.get('input[id="customer.password"]').should('be.visible').type(session_pword)
+//         cy.contains('Confirm').should('be.visible')
+//         cy.get('input[id="repeatedPassword"]').should('be.visible').type(session_pword)
+//         cy.get('[colspan="2"] > .button').should('be.visible').click()
+//     })
+    
+//     it('Should successfully log-in using the created account', () => {
+//         const session_username = Cypress.env('randomusername')
+//         const session_pword = Cypress.env('randompword')
+
+
+//         cy.get('form > :nth-child(2) > .input').should('be.visible').type(session_username)
+//         cy.get(':nth-child(4) > .input').should('be.visible').type(session_pword)
+//         cy.get(':nth-child(5) > .button').should('be.visible').click()
+//         cy.url().should('include','overview.htm')
+//     })
+
+//     it('Should successfully let the user Logout',() => {
+//         const session_username = Cypress.env('randomusername')
+//         const session_pword = Cypress.env('randompword')
+
+//         cy.PB_Login(session_username, session_pword)
+
+//         cy.url().should('include','overview.htm')
+//         cy.contains('Account Services').should('be.visible')
+//         cy.contains('Balance').should('be.visible')
+//         cy.get('#leftPanel > ul > :nth-child(8) > a').should('be.visible').click()
+//         cy.contains('Customer Login').should('be.visible')
+//         cy.url().should('include','index.htm')
+//     } )
+// })
+
+describe('Parabank User Registration with Fixtures and Faker.js', () => {
+    // before(() => {
+    //     const randomUsername = `User${Math.floor(Math.random() * 10000)}`
+    //     const randompword =`pword${Math.floor(Math.random() * 10000)}`
+
+    //     Cypress.env('randomusername', randomUsername)
+    //     Cypress.env('randompword' , randompword)
+
+        
+    // })
+    
+    beforeEach(() =>{
+
+        // const randomUsername = `User${Math.floor(Math.random() * 10000)}`
+        // const randompword =`pword${Math.floor(Math.random() * 10000)}`
+
+        // Cypress.env('randomusername', randomUsername)
+        // Cypress.env('randompword' , randompword)
+
+        cy.visit('https://parabank.parasoft.com/parabank/register.htm')
+    })
+
+    it('Should successfully create a user', () => {
+
+        // const session_username = Cypress.env('randomusername')
+        // const session_pword = Cypress.env('randompword')
+        
+        const NewUser = generateCustomerData()
+        cy.generateFixtureData(NewUser)
+
+        cy.fixture('UserCredentials.json').then((UserCredentials) => {
+            cy.url().should('include','register.htm')
+            cy.get('.title').should('be.visible')
+            cy.contains('First Name').should('be.visible')    
+            cy.get('input[id="customer.firstName"]').should('be.visible').type(UserCredentials.firstName)
+            cy.contains('Last Name').should('be.visible')
+            cy.get('input[id="customer.lastName"]').should('be.visible').type(UserCredentials.lastName)
+            cy.contains('Address').should('be.visible')
+            cy.get('input[id="customer.address.street"]').should('be.visible').type(UserCredentials.address)
+            cy.contains('City').should('be.visible')
+            cy.get('input[id="customer.address.city"]').should('be.visible').type(UserCredentials.city)
+            cy.contains('State').should('be.visible')
+            cy.get('input[id="customer.address.state"]').should('be.visible').type(UserCredentials.state)
+            cy.contains('Zip Code').should('be.visible')
+            cy.get('input[id="customer.address.zipCode"]').should('be.visible').type(UserCredentials.zipCode)
+            cy.contains('Phone #').should('be.visible')
+            cy.get('input[id="customer.phoneNumber"]').should('be.visible').type(UserCredentials.phoneNumber)
+            cy.contains('SSN').should('be.visible')
+            cy.get('input[id="customer.ssn"]').should('be.visible').type(UserCredentials.firstName)
+            cy.contains('Username:').should('be.visible')
+            cy.get('input[id="customer.username"]').should('be.visible').type(UserCredentials.username)
+            cy.contains('Password').should('be.visible')
+            cy.get('input[id="customer.password"]').should('be.visible').type(UserCredentials.password)
+            cy.contains('Confirm').should('be.visible')
+            cy.get('input[id="repeatedPassword"]').should('be.visible').type(UserCredentials.password)
+        })
+
+        cy.get('[colspan="2"] > .button').should('be.visible').click()
+    })
+    
+
+
+    it('Should successfully log-in using the created account', () => {
+
+        cy.wait(2000); 
+        cy.fixture('UserCredentials.json').then((UserCredentials) => {
+            cy.get('form > :nth-child(2) > .input').should('be.visible').type(UserCredentials.username)
+            cy.get(':nth-child(4) > .input').should('be.visible').type(UserCredentials.password)
+        })
+
+        cy.get(':nth-child(5) > .button').should('be.visible').click()
+        cy.url().should('include','overview.htm')
+    })
+
+    it('Should successfully let the user Logout',() => {
+        const session_username = Cypress.env('randomusername')
+        const session_pword = Cypress.env('randompword')
+
+        // cy.PB_Login(session_username, session_pword)
+
+        cy.fixture('UserCredentials.json').then((UserCredentials) => {
+            cy.get('form > :nth-child(2) > .input').should('be.visible').type(UserCredentials.username)
+            cy.get(':nth-child(4) > .input').should('be.visible').type(UserCredentials.password)
+        })
+
+        cy.get(':nth-child(5) > .button').click()
+        cy.url().should('include','overview.htm')
+        cy.contains('Account Services').should('be.visible')
+        cy.contains('Balance').should('be.visible')
+        cy.get('#leftPanel > ul > :nth-child(8) > a').should('be.visible').click()
+        cy.contains('Customer Login').should('be.visible')
+        cy.url().should('contain','index.htm')
+    } )
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,46 +24,84 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('auth', (username, password) => { // FUNCTION OR METHOD --> Then i-call natin sya sa spec or test file natin.
+import 'cypress-xpath';
+
+import { generateCustomerData } from './utils';
+
+Cypress.Commands.add('auth', (username, password) => { // FUNCTION OR METHOD FOR USER LOGIN
     cy.visit('https://www.saucedemo.com/', {timeout: 240000})
       cy.get('[data-test="username"]').type(username)
       cy.get('[data-test="password"]').type(password)
       cy.get('[data-test="login-button"]').click()  
-}); // Try to create other Commands - pwedeng yung add to cart at checkout is gagawan natin ng commands
+});
  
 
-Cypress.Commands.add('AddToCart', () => {
-  //cy.auth('standard_user', 'secret_sauce')
-  //cy.visit('https://www.saucedemo.com/inventory.html', {timeout: 240000})
-  cy.get('[data-test="item-0-title-link"] > [data-test="inventory-item-name"]').click();
-  cy.contains('Sauce Labs Bike Light').should('be.visible')
+Cypress.Commands.add('AddToCart', () => { // FUNCTION FOR ADDING A SAMPLE ITEM TO CART
+  cy.get('[data-test="item-3-title-link"] > [data-test="inventory-item-name"]').click()
+  cy.contains('Test.allTheThings()')
   cy.get('[data-test="add-to-cart"]').should('be.visible').should('not.be.disabled').click()
   cy.get('.shopping_cart_badge').should('contain', '1')
   cy.get('[data-test="shopping-cart-link"]').click()
   cy.get('.cart_item').should('have.length', 1)
-  cy.get('.inventory_item_name').should('contain', 'Sauce Labs Bike Light')
+  cy.get('[data-test="inventory-item-name"]').should(($el) => {//Checker if there is an existing product added to cart
+    const text = $el.text();
+    expect(text.includes('Sauce Labs') || text.includes('Test.allTheThings()')).to.be.true;
+  });
+  
 });
 
-Cypress.Commands.add('Checkout', () => {
-  //cy.auth('standard_user', 'secret_sauce')
-  //cy.visit('https://www.saucedemo.com/inventory.html', {timeout: 240000})
-  //cy.AddToCart()
+Cypress.Commands.add('Checkout', () => { // FUNCTION FOR CHECKING OUT A SAMPLE ITEM 
+  //Opening cart to proceed to first checkout step
   cy.get('[data-test="shopping-cart-link"]').click()
   cy.get('[data-test="title"]').should('be.visible')
   cy.contains('Your Cart').should('be.visible')
-  cy.contains('Sauce Labs Bike Light').should('be.visible')
+  cy.get('[data-test="inventory-item-name"]').should(($el) => {//Checker if there is an existing product added to cart
+    const text = $el.text();
+    expect(text.includes('Sauce Labs') || text.includes('Test.allTheThings()')).to.be.true;
+  });
   cy.get('[data-test="checkout"]').should('not.be.disabled').click()
+
+  //Add sample customer details to checkout
+  cy.url().should('include','saucedemo.com/checkout-step-one.html')
   cy.get('[data-test="title"]').should('be.visible')
   cy.contains('Checkout: Your Information').should('be.visible')
   cy.get('[data-test="firstName"]').should('be.visible').type('Juan')
   cy.get('[data-test="lastName"]').should('be.visible').type('Dela Cruz')
   cy.get('[data-test="postalCode"]').should('be.visible').type('1116')
   cy.get('[data-test="continue"]').should('not.be.disabled').click()
+
+  //Overview of checkout
   cy.contains('Checkout: Overview').should('be.visible')
-  cy.get('[data-test="finish"]').should('be.visible').click()
+  cy.url().should('include','saucedemo.com/checkout-step-two.html')
+  cy.get('[data-test="payment-info-value"]').should('be.visible').should('contain', 'SauceCard #31337')
+  cy.get('[data-test="finish"]').should('be.visible').should('not.be.disabled').click()
+
+  // Assertion of confirmation message
   cy.get('[data-test="title"]').should('be.visible')
   cy.contains('Checkout: Complete!').should('be.visible')
   cy.get('[data-test="pony-express"]').should('be.visible')
   cy.contains('Thank you for your order!').should('be.visible')
+  cy.screenshot()
   cy.get('[data-test="back-to-products"]').should('be.visible').click()
+
 })
+
+//Login function for Parabank activity
+Cypress.Commands.add('PB_Login', (Username, pword) =>{
+  cy.get('form > :nth-child(2) > .input').type(Username)
+  cy.get(':nth-child(4) > .input').type(pword)
+  cy.get(':nth-child(5) > .button').click()
+})
+
+Cypress.Commands.add('PB_LoginFixtures', (NewUser) =>{
+  cy.get('form > :nth-child(2) > .input').type(NewUser.username)
+  cy.get(':nth-child(4) > .input').type(NewUser.password)
+  cy.get(':nth-child(5) > .button').click()
+})
+
+Cypress.Commands.add('generateFixtureData' , (NewUser) => {
+  // let generatedSampleData = generateTestData()
+  //cy.writeFile('cypress/fixtures/UserCredentials.json', generatedSampleData);
+  cy.writeFile('cypress/fixtures/UserCredentials.json', NewUser);
+});
+
