93de44b336f74b24d10be76e0e3b755e1f1784b1
MaNaMee <joshuaazarcon22@gmail.com>
2025-04-14 11:26:11 +0800

    Update: Updated command.js, utils.js, added AutomationExercise.cy.js, RegisteredUser.json for AutomationExercise.com Activity

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -2,10 +2,10 @@ import { faker } from '@faker-js/faker';
 
 export function generateCustomerData() {
 
-  const randomNumber = faker.string.numeric(2);
-  const usernameBase = faker.internet.username();
-  const username = `${usernameBase}${randomNumber}`;
-  //const username = usernameBase + randomNumber;
+   const randomNumber = faker.string.numeric(2);
+   const usernameBase = faker.internet.username();
+   const username = `${usernameBase}${randomNumber}`;
+  // username : usernameBase + randomNumber;
   return {
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
@@ -19,4 +19,36 @@ export function generateCustomerData() {
     username: username,
     password: `pword${Math.floor(Math.random() * 10000)}`,
   };
+}
+
+
+export function generateCustomerInformation(){
+
+    const allowedCountries = ['India','United States', 'Canada', 'Australia', 'Israel', 'New Zealand', 'Singapore']
+    const country = faker.helpers.arrayElement(allowedCountries);
+    const RandomYear = faker.number.int({min: 1900, max: 2021})
+    const expiryDate = faker.date.future()
+    return {
+     username : faker.person.fullName(),
+     email: faker.internet.email(),
+     fname : faker.person.firstName(),
+     lname : faker.person.lastName(),
+     password : faker.internet.password(),
+     address : faker.location.streetAddress(),
+     address2 : faker.location.secondaryAddress(),
+     days: faker.number.int({ min:1, max: 31}),
+     month : faker.date.month(),
+     year : RandomYear.toString(),
+     random_country : country,
+     state : faker.location.state(),
+     city : faker.location.city(),
+     zipcode : faker.location.zipCode(),
+     mobile_number : faker.phone.number(),
+     expiryDate : faker.date.future(),
+     cc_number : faker.finance.creditCardNumber(),
+     cc_cvv : faker.finance.creditCardCVV(),
+     cc_exp_month : String(expiryDate.getMonth() + 1).padStart(2, '0'),
+     cc_exp_year : expiryDate.getFullYear()
+    };
+
 }
